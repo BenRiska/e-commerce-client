cf8b4c439037fd9c80800d176412fe99abe226ff
bentaylor <ben10taylor10@hotmail.com>
2021-02-12 12:58:08 +0000

    initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+
+import React from 'react';
+import reactDOM from 'react-dom';
+import "./index.css"
+
+
+const App = () => {
+  return <div>Hello World</div>
+}
+
+reactDOM.render(<App />, document.getElementById('root'))
---
+++ webpack.common.js
@@ -0,0 +1,31 @@
+module.exports = {
+    entry: {
+        main: "./src/index.js"
+    },
+    plugins: [
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                use: ["html-loader"]
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.(svg|png|jpg|jpeg|gif)$/,
+                use: {
+                    loader: "file-loader",
+                    options: {
+                        name: "[name].[hash].[ext]",
+                        outputPath: "imgs"
+                    }
+                }
+            }
+        ]
+    },
+    devtool: 'eval-cheap-module-source-map'
+}
